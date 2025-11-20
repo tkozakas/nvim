97560fdbfd19a42cbb6616e5d23471d81c068df4
Tomas Kozakas <tomas.kozakas@vinted.com>
2025-11-20 08:58:46 +0200

    Update options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,61 +1,70 @@
+-- Leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
 
-vim.opt.number = true
+-- General behavior
+vim.opt.autoread = true -- auto update file if changed outside of nvim
+vim.opt.autochdir = true -- auto change directory to current file's directory
+vim.opt.exrc = true
 vim.opt.mouse = "a"
 vim.opt.showmode = false
+vim.opt.swapfile = false
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true -- persistent undo history
+vim.opt.updatetime = 250 -- save swap file and trigger CursorHold
+
+-- Clipboard
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
+
+-- Display
+vim.opt.background = "dark"
 vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.o.incsearch = true -- enable highlighting search in progress
-vim.opt.signcolumn = "yes"
-vim.o.termguicolors = true -- enable 24-bit colors
-vim.o.updatetime = 200 -- save swap file with 200ms debouncing
-vim.o.autoread = true -- auto update file if changed outside of nvim
-vim.o.undofile = true -- persistant undo history
-vim.o.number = true -- enable line numbers
-vim.o.relativenumber = false -- enable relative line numbers
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.cmdheight = 0
+vim.opt.cursorline = true
+vim.opt.inccommand = "split"
+vim.opt.laststatus = 3
 vim.opt.list = true
 vim.opt.listchars = { tab = '▏ ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = "split"
+vim.opt.number = true
+vim.opt.relativenumber = false
 vim.opt.scrolloff = 999
-vim.opt.hlsearch = false
-vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true -- enable 24-bit colors
 vim.opt.wrap = false
-vim.opt.swapfile = false
-vim.opt.autochdir = true -- Auto change directory to current file's directory
-vim.opt.laststatus = 3
-vim.opt.cmdheight = 0
-vim.opt.exrc = true
+
+local Maxline = 80
+vim.cmd("set colorcolumn=" .. Maxline)
+
+-- Search
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.incsearch = true -- enable highlighting search in progress
+vim.opt.smartcase = true
+
+-- Indentation
+vim.opt.autoindent = true -- copy indent from current line when starting new line
+vim.opt.expandtab = true -- use appropriate number of spaces with tab
+vim.opt.shiftwidth = 2 -- controls number of spaces when using >> or << commands
+vim.opt.smartindent = true -- indenting correctly after {
+vim.opt.softtabstop = 2 -- how many spaces tab inserts
+vim.opt.tabstop = 2 -- how many spaces tab inserts
+
+-- Completion
+vim.opt.completeopt = "menu,menuone,noselect,preview" -- omnicomplete options for popup menu
+vim.opt.pumheight = 15 -- max height of completion menu
+
+-- UI
+vim.opt.winborder = "rounded" -- rounded border
+
+-- Diagnostics
 vim.diagnostic.config({
 	virtual_text = {
 		spacing = 4,
 		prefix = "●",
 	},
-}) 
-vim.opt.background = "dark"
-
-vim.o.tabstop = 2 -- how many spaces tab inserts
-vim.o.softtabstop = 2 -- how many spaces tab inserts
-vim.o.shiftwidth = 2 -- controls number of spaces when using >> or << commands
-vim.o.expandtab = true -- use appropriate number of spaces with tab
-vim.o.smartindent = true -- indenting correctly after {
-vim.o.autoindent = true -- copy indent from current line when starting new line
-vim.o.scrolloff = 8 -- always keep 8 lines above/below cursor unless at start/end of file
-
-
-vim.o.completeopt = "menu,menuone,noselect,preview" -- omnicomplete options for popup menu
-vim.o.pumheight = 10 -- max height of completion menu
-vim.o.winborder = "rounded" -- rounded border
-vim.o.showmode = false -- disable showing mode below statusline
-local Maxline = 80
-vim.cmd("set colorcolumn=" .. Maxline)
+})
