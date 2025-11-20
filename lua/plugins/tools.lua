e1d164a423ea1b3cd658933ffdded258e090094e
Tomas Kozakas <tomas.kozakas@vinted.com>
2025-11-20 08:48:06 +0200

    Update

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,5 +1,25 @@
 return {
 	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+			linehl = false,
+			numhl = true,
+			current_line_blame = false,
+			current_line_blame_opts = {
+				delay = 1000,
+			},
+		},
+	},
+	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
